0d1d303d68a3b453b09f40aa0638ab1fc8d08c3c
Leo <273127393@qq.com>
2016-04-27 17:53:10 +0800

    [init] init

---
+++ FrameWork/LMEmbeddedViewController.h
@@ -0,0 +1,16 @@
+//
+//  LMEmbeddedViewController.h
+//  LMAlertViewDemo
+//
+//  Created by Lee McDermott on 19/11/2013.
+//  Copyright (c) 2013 Bestir Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PCAlertView.h"
+
+@interface LMEmbeddedViewController : UIViewController
+
+@property (weak, nonatomic) PCAlertView *alertView;
+
+@end
